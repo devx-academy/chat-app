1d7cca1da700884bd096f14ad14b59ca90cbc77b
Jakub Matena <jakub.matena@renomia.cz>
2020-07-18 01:15:11 +0200

    rxjs - received messages connector

--- src/components/MessageWindow.jsx
+++ src/components/MessageWindow.jsx
@@ -1,28 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ChatAppContext } from '../contexts/ChatAppContextProvider'
+import React from 'react'
+import receivedMessageConnector from '../hocs/receivedMessageConnector'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
-export default () => {
-  const chatContext = useContext(ChatAppContext)
-  const [messages, setMessages] = useState([])
-
-  useEffect(() => {
-    const msgSubscription = chatContext.receivedMessages$.subscribe((msgs) => {
-      setMessages(msgs)
-    })
-
-    return () => msgSubscription.unsubscribe()
-  }, [chatContext])
-
+const MessageWindow = ({ messages }) => {
   return (
     <div>
-      <div>
-        <button onClick={() => {
-          chatContext.receivedMessages$.next([...messages, 'jedna'])
-        }}>
-          new message
-        </button>
-      </div>
       {messages.map((msg) => <div>{`zprava: ${msg}`}</div>)}
     </div>
   )
 }
+
+export default receivedMessageConnector((stream$) => stream$.pipe(
+  map((msgs) => msgs.map((msg) => `${msg} - ${new Date()}`))
+))(MessageWindow)
