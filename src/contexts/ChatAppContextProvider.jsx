699407ef257b3bc1fafb86d1531916f5e020aa98
Jan Mares <jan.mares@dtforce.com>
2020-07-21 22:43:08 +0200

    napojeni na BE

--- src/contexts/ChatAppContextProvider.jsx
+++ src/contexts/ChatAppContextProvider.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import { Subject, BehaviorSubject } from 'rxjs'
-import { STOMP_STATES } from '../services/api'
+import { getStompClient, STOMP_STATES } from '../services/api'
 
 export const ChatAppContext = React.createContext()
 
 const initChatContext = (username) => {
   let receivedMessages = []
+  let subscription;
 
   const chatConnectionState$ = new BehaviorSubject(STOMP_STATES.DISCONNECTED)
   const receivedMessages$ = new BehaviorSubject(receivedMessages)
+  const subscription$ = new Subject()
   const sendMessages$ = new Subject()
+
+  const receiveMessage = (message) => {
+    receivedMessages = [...receivedMessages, message]
+    receivedMessages$.next(receivedMessages)
+  }
+
+  const stompClient = getStompClient("ws://localhost:8080/gs-guide-websocket", chatConnectionState$)
+  chatConnectionState$.subscribe(value => {
+    console.log(value);
+    if (value === STOMP_STATES.CONNECTED) {
+      subscription$.next(
+        stompClient.subscribe("/topic/greetings", function (frame) {
+          console.log(JSON.parse(frame.body))
+          receiveMessage(JSON.parse(frame.body));
+        })
+      )
+    }
+  })
+
+  sendMessages$.subscribe(value => {
+    stompClient.publish({destination: '/app/hello', body: JSON.stringify({
+        body: value,
+        sender: username,
+        receiver: '#all'
+    })});
+  })
 
   return {
     user: {
@@ -17,13 +45,12 @@ const initChatContext = (username) => {
     },
     connection: {
       connectionState$: chatConnectionState$,
+      client: stompClient,
+      subscription: subscription
     },
     receivedMessages$,
     sendMessages$,
-    receiveMessage: (message) => {
-      receivedMessages = [...receivedMessages, message]
-      receivedMessages$.next(receivedMessages)
-    },
+    receiveMessage: receiveMessage,
     sendMessage: (message) => sendMessages$.next(message)
   }
 }
